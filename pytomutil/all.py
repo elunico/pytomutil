1d2f76442280c2b25d35b338fc60931ab003f6f6
Thomas Povinelli <elunico@users.noreply.github.com>
2024-11-14 08:02:19 -0500

    fixed some edge cases in key_merge

--- pytomutil/all.py
+++ pytomutil/all.py
@@ -3,3 +3,4 @@ from meta import *
 from numeric import *
 from SRGBColor import *
 from vec import *
+from dicts import *
--- pytomutil/numeric.py
+++ pytomutil/numeric.py
@@ -8,3 +8,7 @@ def qerp(a: float, b: float, c: float, t: float) -> float:
 
 def cerp(a: float, b: float, c: float, d: float, t: float) -> float:
     return lerp(qerp(a, b, c, t), qerp(b, c, d, t), t)
+
+
+def terp(a: float, b: float, c: float, d: float, e: float, t: float) -> float:
+    return lerp(cerp(a, b, c, d, t), cerp(b, c, d, e, t), t)
