d7ea2d9c38bf540f0fbfdf2406b62932742de246
elunico <elunico@users.noreply.github.com>
2025-06-22 14:09:27 -0400

    version incr

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="pytomutil-elunico",
-    version="0.0.8",
+    version="0.0.9",
     author="Thomas Povinelli",
     author_email="tompov227@gmail.com",
     description="Library for parsing and writing Simple Property List (SPL) files ",
